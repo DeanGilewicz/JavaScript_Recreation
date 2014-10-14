acda4eaac418972daf67d4873e48001795c29317
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-13 23:28:35 -0400

    first function created

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,6 +24,7 @@ dean.random = function (min, max) {
 dean.random();
 
 
+
 // First Function - Returns the first element of an array.
 
 var test_array1 = ['hello', 2, 'twitter', 7, 'apple', 12];
@@ -33,63 +34,3 @@ dean.first = function (x) {
 }
 
 console.log(dean.first(test_array1));
-
-
-
-// Last Function - Returns the last element of an array.
-
-
-var test_array2 = ['Steve', 'Lee', 'Jane', 'Sue', 'Graham', 'Pam', 'Heather', 'John'];
-
-dean.last = function () {
-
-	return test_array2[test_array2.length - 1];
-}
-
-console.log(dean.last(test_array2));
-
-
-// Max Function - Returns the max value in a list
-
-var test_array3 = [54, 32, 231253, 2341, 12, 4, 5875, 3];
-
-dean.highest = function (x) {
-	var max = 0;
-	for (i = 0; i < x.length; i ++) {
-	    if (x[i] > max) {
-	        max = x[i];
-	    }
-	}
-	return max;
-};
-console.log(dean.highest(test_array3));
-
-
-// Min Function - Returns the minimum value in a list
-
-var test_array4 = [423, 32, 77, 3, 489, 49, 6234];
-
-dean.lowest = function (x) {
-	var min = 0;
-	for (i = 0; i < x.length; i ++) {
-			if (x[i] < min) {
-					min = x[i];
-			}
-	}
-	return min;
-};
-console.log(dean.lowest(test_array4));
-
-
-// Size Function - Returns the number of values in a list
-
-var test_array5 = {temp:'cool', age:20, color:'blue', street:99, food:'ice cream', pets:13, holiday:'halloween'};
-
-dean.value_num = function (obj) {
-	var num = obj.value;
-	for (i = 0; i < obj.length; i ++) {
-    return num;
-	}
-}
-
-console.log(dean.value_num(test_array5));
