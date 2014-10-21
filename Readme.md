4b129b55fb85cc51bc57daf8c701bab59fe3df76
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-20 23:51:36 -0400

    updated read me

--- Readme.md
+++ Readme.md
@@ -1,27 +1,27 @@
 ## JavaScript Recreations
 
-The following JavaScript functions have been created in the object dean.
+### The following JavaScript functions have been created in the object dean.
 
 To call the function you should use "dean." and then type the name of the function you want to use followed by (). Required parameters are listed below.
 
-1) dean.map(); ... produces a new array of values from current array.
+1) dean.map(); ... produces a new array of values from current array. Parameter 1 needs to be an array, parameter 2 needs to be a function.
 
 2) dean.now(); ... returns the current time. No parameters needed.
 
 3) dean.greeting(); ... returns a greeting message (Good Morning, Good Afternoon, Good Evening, Good Night) based on the time of day. No parameters needed.
 
-4) dean.random(); ... returns a random number between 0 and integer you declare as the parameter. If two parameters are entered then random number will be returned using 1st paramenter as min value, and second as max value.
+4) dean.random(); ... returns a random number between 0 and integer you declare as the parameter. If two parameters are entered then random number will be returned using 1st paramenter as min value, and second as max value. 1 paramenter requied.
 
-5) dean.first(); ... returns first element in an array.
+5) dean.first(); ... returns first element in an array. Parameter needs to be an array.
 
-6) dean.last(); ... returns last element in an array.
+6) dean.last(); ... returns last element in an array. Parameter needs to be an array.
 
-7) dean.highest(); ... returns highest value in an array.
+7) dean.highest(); ... returns highest value in an array. Parameter needs to be an array.
 
-8) dean.lowest(); ... returns lowest value in an array.
+8) dean.lowest(); ... returns lowest value in an array. Parameter needs to be an array.
 
-9) dean.object(); ... converts arrays into objects. Pass either a single list of key:value pairs, of a list of keys. and a list of values
+9) dean.object(); ... converts arrays into objects. Pass either a single list of key:value pairs, or a list of keys and a list of values.
 
-10) dean.maxTwo(); ... returns highest number of 2 integers.
+10) dean.maxTwo(); ... returns highest number of 2 integers entered as parameters.
 
-11) dean.maxTwo(); ... returns highest number of 3 integers.
+11) dean.maxTwo(); ... returns highest number of 3 integers entered as parameters.
--- app/scripts/dean.js
+++ app/scripts/dean.js
@@ -78,7 +78,7 @@
   };
 
 
-  // Highest Function - Returns the max value in a list
+  // Highest Function - Returns the max value in a list.
   dean.highest = function (x) {
     var max = 0;
     for (i = 0; i < x.length; i ++) {
@@ -90,34 +90,38 @@
   };
 
 
-  // Lowest Function - Returns the minimum value in a list ** ONLY COMPARES 1st AND SECOND VALIES AT THE MOMENT
-  // var test_array = [50, 60, 77, 9, 489, 6, 6234];
-  //
-  // dean.lowest = function (x) {
-  //
-  //   for (i = 0; i < x.length; i++) {
-  //     var current = x[i];
-  //     var next = x[i + 1];
-  //
-  //     if (x = null) {
-  //       return "This is not an array";
-  //
-  //     } else if (current > next) {
-  //         current = next;
-  //         return next;
-  //
-  //     } else {
-  //         next = current;
-  //         return current;
-  //     }
-  //   }
-  // };
-  //
-  // console.log(dean.lowest(test_array));
+  // Lowest Function - Returns the minimum value in a list ** ONLY COMPARES 1st AND SECOND VALUES AT THE MOMENT
+  var array = [50, 60, 77, 9, 489, 6, 6234];
+
+  dean.lowest = function (x) {
+
+    // iterate through data and sort data from smallest to largest
+    // return 1st value
+
+    var data = [];
+
+    for (i = 0; i < x.length; i++) {
+
+    var current = x[i];
+
+    var next = x[i + 1];
+
+      if (x = null) {
+        return "This is not an array";
+
+      } else if (current < next) {
+          current = current;
+          return current;
+
+      } else {
+          current = next;
+          return next;
+      }
+    }
+  };
 
 
-
-  // Object Function - converts arrays into objects. Pass either a single list of key:value pairs, of a list of keys. and a list of values
+  // Object Function - converts arrays into objects. Pass either a single list of key:value pairs, or a list of keys and a list of values.
   dean.object = function(list, values) {
       if (list == null) return {};
       var result = {};
@@ -157,72 +161,3 @@
 
 
 }());
-
-
-
-/****************************************************************************************************************
-Question 3: Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
-
-*****************************************************************************************************************/
-// dean.vowel = function (x) {
-//   if (x.string === "a" || "e" || "i" || "o" || "u") {
-//     return "It is a vowel";
-//   }
-//   else {
-//     return "No vowel here";
-//   }
-// };
-// console.log(vowel(x));
-
-
-/****************************************************************************************************************
-Question 4: Write a function translate() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
-
-*****************************************************************************************************************/
-
-
-
-/****************************************************************************************************************
-Question 5: Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
-
-*****************************************************************************************************************/
-
-
-
-
-/****************************************************************************************************************
-Question 6: Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
-
-
-*****************************************************************************************************************/
-
-
-/****************************************************************************************************************
-Question 7: Represent a small bilingual lexicon as a Javascript object in the following fashion {"merry":"god", "christmas":"jul", "and":"och", "happy":gott", "new":"nytt", "year":"år"} and use it to translate your Christmas cards from English into Swedish.
-
-*****************************************************************************************************************/
-
-
-
-/****************************************************************************************************************
-Question 8: Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
-
-
-*****************************************************************************************************************/
-
-
-
-/****************************************************************************************************************
-Question 9: Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
-
-
-*****************************************************************************************************************/
-
-
-
-
-/****************************************************************************************************************
-Question 10: Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
-
-
-*****************************************************************************************************************/
